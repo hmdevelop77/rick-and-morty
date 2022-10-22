4f07c9fabf87495937ee5314e3dee2b3ee7b650a
Oussama Hamdani <hmweb77@gmail.com>
2022-10-22 16:27:32 +0100

    lastUpdate

--- index.js
+++ index.js
@@ -13,33 +13,40 @@ document.getElementById("restart-button").onclick = () => {
 document.getElementById("levelpass-button").onclick = () => {
   document.getElementById("restart-button").style.visibility = "hidden";
   document.getElementById("levelpass-button").style.visibility = "hidden";
-  startNewGame();
+  nextLevel();
 };
+
 let currentGame;
+let level = 1
+let reachedFirstPlanet = false;
 class Game {
   constructor() {
     this.ship = {};
     this.aliens = [];
-    this.planet = [];
+    this.planet = null;
     this.aliensFrequency = 0;
-    this.gamerun= false ;
+    this.gamerun = false;
     this.gameOver = false;
-    this.levelPassed = true;
+    this.level = 1;
     this.intervalId = null;
+    this.planet1 = {};
+    this.planet2 = {}
   }
 }
 function startGame() {
-  if(!currentGame){
-  currentGame = new Game();
-  let currentShip = new Ship();
-  currentGame.ship = currentShip;
-  let currentPlanet = new Planet();
-  currentGame.planet = currentPlanet;
-  currentGame.intervalId = setInterval(() => {
-    updateCanvas();
-  }, 1000 /30 );
-  } 
-};
+  if (!currentGame) {
+    //   let level ;
+    //  currentGame.level = level;
+    currentGame = new Game();
+    let firstPosOfShip = new Ship(50, 650);
+    currentGame.ship = firstPosOfShip;
+    let firstPlanet = new Planet(950, 20, "image/planet-image.jpeg", 100, 100, "first");
+    currentGame.planet1 = firstPlanet;
+    currentGame.intervalId = setInterval(() => {
+      updateCanvas();
+    }, 1000 / 30);
+  }
+}
 function detectCollision(alien) {
   return !(
     currentGame.ship.x > alien.x + alien.width - 25 ||
@@ -47,7 +54,7 @@ function detectCollision(alien) {
     currentGame.ship.y > alien.y + alien.height - 20 ||
     currentGame.ship.y + currentGame.ship.height - 30 < alien.y
   );
-};
+}
 function checkGameOver() {
   currentGame.aliens = [];
   context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
@@ -57,71 +64,79 @@ function checkGameOver() {
   context.fillStyle = "rgb(114, 239, 113)";
   context.font = "100px Ricks";
   context.fillText("GAME OVER", 200, 400);
- currentGame = null
-};
+  currentGame = null;
+}
 function checkReachPlanet() {
   context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
   clearInterval(currentGame.intervalId);
   document.getElementById("levelpass-button").style.visibility = "visible";
   context.shadowColor = "black";
   context.fillStyle = "rgb(114, 239, 113)";
-  context.font = "100px Ricks";
-  context.fillText("LEVEL PASS", 200, 400);
-  // nextLevel ()
-};
-// function nextLevel (){
-//   context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
-//   clearInterval(currentGame.intervalId);
-// }
-currentGame = null ;
-function startNewGame() {
-  // context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
-  // if(!currentGame){
-    startGame();
-  // currentGame = new Game();
-  // let currentShip = new Ship();
-  // currentGame.ship = currentShip;
-  // let currentPlanet = new Planet();
-  // currentGame.planet = currentPlanet;
-  // currentGame.intervalId = setInterval(() => {
-  //   updateCanvas();
-  // }, 1);
-  //}
-};
+  context.font = "45px Ricks";
+  context.fillText("CONGRATULATIONS YOU LANDED SALEFY", 110, 350);
+  reachedFirstPlanet = true;
+ 
+}
+function nextLevel() {
+  context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
+  clearInterval(currentGame.intervalId);
+  let secondPosOfShip = new Ship(900, 40);
+  currentGame.ship = secondPosOfShip;
+  let secondPlanet = new Planet(30, 600, "image/second planet.png", 150, 150, "second");
+  currentGame.planet2 = secondPlanet;
+  currentGame.intervalId = setInterval(() => {
+    updateCanvas();
+  }, 1000 / 30);
+ 
+}
+function winGame() {
+  context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
+  clearInterval(currentGame.intervalId);
+  context.shadowColor = "black";
+  context.fillStyle = "rgb(114, 239, 113)";
+  context.font = "45px Ricks";
+  context.fillText("CONGRATULATIONS YOU WON", 110, 350);
+}
 function updateCanvas() {
   context.clearRect(0, 0, spaceCanvas.clientWidth, spaceCanvas.clientHeight);
   currentGame.ship.draw();
-  currentGame.planet.draw();
-  if (detectCollision(currentGame.planet)) {
-    currentGame.gameOver = true;
-    checkReachPlanet();
-  };
-  if (detectCollision(currentGame.planet)) {
-    currentGame.gameOver = true;
-    currentGame.aliensFrequency = 0;
+  if (level === 1) {
+    currentGame.planet1.draw();
+    if (detectCollision(currentGame.planet1)) {
+      currentGame.gameOver = true;
+      currentGame.aliensFrequency = 0;
       currentGame.aliens = [];
-    startNewGame();
-  };
-  
-  currentGame.aliensFrequency += 5;
+      level = 2;
+      checkReachPlanet();
+    }
+  } else if (level === 2 && currentGame.planet2.name !== undefined) {
+    currentGame.planet2.draw()
+    if (detectCollision(currentGame.planet2)) {
+      currentGame.gameOver = true;
+      currentGame.aliensFrequency = 0;
+      currentGame.aliens = [];
+      winGame();
+    }
+  }
+
+  currentGame.aliensFrequency += 10;
   currentGame.aliens.forEach((alien) => {
-    alien.x += 5 // speed of aliens
+    alien.x += 7; // speed of aliens
     alien.drawAliens();
     if (detectCollision(alien)) {
       currentGame.gameOver = true;
       currentGame.aliensFrequency = 0;
       currentGame.aliens = [];
       checkGameOver();
-    };
+    }
   });
   if (currentGame.aliensFrequency % 120 === 60) {
     const randomAlienX = 0;
     const randomAlienY = Math.floor(Math.random() * 600);
     const currentAliens = new Aliens(randomAlienX, randomAlienY);
     currentGame.aliens.push(currentAliens);
-  };
-};
+  }
+}
 document.addEventListener("keydown", (keyboardEvent) => {
   currentGame.ship.moveShip(keyboardEvent.key);
 });
-
--- planet.js
+++ planet.js
@@ -1,27 +1,17 @@
-
-let planetImg = new Image
 class Planet {
-  constructor() {
-    this.x = 950;
-    this.y = 20;
-    this.width = 100;
-    this.height = 100;
+  constructor(x, y, image, width, height, name) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.width = width; 
+    this.height = height; 
     this.image = new Image();
-    this.image.src = "image/planet-image.jpeg";
+    this.image.src = image;
     this.image.onload = () => {
       this.draw();
     };
-   }
+  }
   draw() {
     context.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
-} 
-// drawPlanet(level) {
-//     if (level === 1) {
-//       planetImg.src = "./image/planet-image.jpeg";
-//     } else if (level === 2) {
-//       planetImg.src = "./image/planète-étrangère-exotique-originale-de-géant-gaz-vert-d-imagination-avec-r-bleu-132260247.jpeg";
-//     } else if (level === 3) {
-//       planetImg.src = "./image/S1e10_hideout_planet.webp";
-//     }
-//     context.drawImage(planetImg, this.x, this.y, 50, 50);
+}
